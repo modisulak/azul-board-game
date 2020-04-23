b94f63b75700119908570db01206e34eecb267f5
tpfundt-at-688878875506 <fHW3WRcghxDmaslOapk5bzw/4obI7W0H51riO7+tpsw=>
2020-04-23 14:39:05 +1000

    Alter menu input to take string, refactor switch statement to if/else. Refactor.

--- include/main.h
+++ include/main.h
@@ -1,4 +1,20 @@
+#ifndef APT_A2_MAIN_H
+#define APT_A2_MAIN_H
+
 #include <iostream>
 #include <string> 
 #include <iostream>
-#include <fstream> 
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::ifstream;
+
+#define NEW_GAME  "1"
+#define LOAD_GAME "2"
+#define CREDITS   "3"
+#define QUIT      "4"
+
+#endif //APT_A2_MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,60 @@
 #include "../include/main.h"
 
-using std::cout; 
-using std::cin; 
-using std::endl;
-using std::string;
-using std::ifstream;
+void displayMenu();
 
-void displayMenu(); 
 void displayCredits();
 
-const string filename = "credits.txt";
-
-int input; 
-
-bool gameExit = false;
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     cout << "Welcome to Azul!" << endl;
-    cout << "-------------------" << endl << endl;
-    
-    while (gameExit != true) {
+    cout << "-------------------" << endl;
+
+    bool gameExit = false;
+    while (!gameExit) {
         displayMenu();
         std::cout << "> ";
-        cin >> input; 
-        
-        switch(input) { 
-            case 1: 
+
+        string input;
+        cin >> input;
+
+        if (cin.good()) {
+            if (input == NEW_GAME) {
                 cout << "To-do" << endl;
-                break;
-            case 2: 
+            } else if (input == LOAD_GAME) {
                 cout << "To-do" << endl;
-                break;
-            case 3: 
-                 displayCredits();
-                 break;
-            case 4: 
-                gameExit = true;  
-                break;   
+            } else if (input == CREDITS) {
+                displayCredits();
+            } else if (input == QUIT) {
+                gameExit = true;
+            } else {
+                cout << "Selection is invalid. Please try again." << endl;
+            }
         }
-        
     }
-
-    return EXIT_SUCCESS; 
-    
+    return EXIT_SUCCESS;
 }
 
-void displayMenu() { 
-     cout << "\n";
-     cout << "Menu" << endl;
-     cout << "----" << endl;
-     cout << "1. New Game" << endl;
-     cout << "2. Load Game" << endl;
-     cout << "3. Credits (Show Student Information" << endl;
-     cout << "4. Quit" << endl << endl;
+void displayMenu() {
+    cout << endl << "Menu" << endl;
+    cout << "----" << endl;
+    cout << "1. New Game" << endl;
+    cout << "2. Load Game" << endl;
+    cout << "3. Credits (Show Student Information)" << endl;
+    cout << "4. Quit" << endl << endl;
 }
 
-void displayCredits() { 
-    string line;
-    ifstream file(filename); 
-    cout << "------------------------------------" << endl; 
-     if(file.is_open()) {
-        while(!file.eof()) {
-            getline(file,line);
+void displayCredits() {
+    string filename = "../resources/credits.txt";
+    ifstream file;
+    file.open(filename);
+
+    cout << "-------------------------------------" << endl;
+    if (file.is_open()) {
+        while (!file.eof()) {
+            string line;
+            getline(file, line);
             cout << line << endl;
-            
-             }
+        }
         file.close();
     }
-    cout << "-------------------------------------" << endl; 
+    cout << "-------------------------------------" << endl;
 }
