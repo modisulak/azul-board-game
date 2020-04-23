8cc0ea85bd5963f5dc6242a9f131578ddcbcba8c
[Moditha Sulakshana s3756843] <s3756843@student.rmit.edu.au>
2020-04-23 12:44:51 +1000

    Implemented the displayMenu() and displayCredits(). Created a credits.txt file with names and info.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,71 @@
-#include <main.h>
+#include "../include/main.h"
+
+using std::cout; 
+using std::cin; 
+using std::endl;
+using std::string;
+using std::ifstream;
+
+void displayMenu(); 
+void displayCredits();
+
+const string filename = "credits.txt";
+
+int input; 
+
+bool gameExit = false;
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
+    cout << "Welcome to Azul!" << endl;
+    cout << "-------------------" << endl << endl;
+    
+    while (gameExit != true) {
+        displayMenu();
+        std::cout << "> ";
+        cin >> input; 
+        
+        switch(input) { 
+            case 1: 
+                cout << "To-do" << endl;
+                break;
+            case 2: 
+                cout << "To-do" << endl;
+                break;
+            case 3: 
+                 displayCredits();
+                 break;
+            case 4: 
+                gameExit = true;  
+                break;   
+        }
+        
+    }
+
+    return EXIT_SUCCESS; 
+    
+}
+
+void displayMenu() { 
+     cout << "\n";
+     cout << "Menu" << endl;
+     cout << "----" << endl;
+     cout << "1. New Game" << endl;
+     cout << "2. Load Game" << endl;
+     cout << "3. Credits (Show Student Information" << endl;
+     cout << "4. Quit" << endl << endl;
+}
+
+void displayCredits() { 
+    string line;
+    ifstream file(filename); 
+    cout << "------------------------------------" << endl; 
+     if(file.is_open()) {
+        while(!file.eof()) {
+            getline(file,line);
+            cout << line << endl;
+            
+             }
+        file.close();
+    }
+    cout << "-------------------------------------" << endl; 
 }
