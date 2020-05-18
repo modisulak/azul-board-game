9d8be9c884d2a11425e3e2e1e6b9cb93c5bda7fa
[Moditha Sulakshana s3756843] <s3756843@student.rmit.edu.au>
2020-05-18 17:03:47 +1000

    updates the test cases and added failed test case.

--- include/Types.h
+++ include/Types.h
@@ -8,45 +8,46 @@
 #include <utility>
 #include <vector>
 #include <algorithm>
+#include <array>
 
-#define BLANK_SPACE             ' '
-#define EMPTY                   '.'
-#define FIRST_PLAYER_TILE       'F'
-#define INPUT_TAB               "> "
-#define SAVE_PATH               "../save_files/"
-#define FILE_EXTENSION          ".txt"
-#define TURN                    "TURN"
-#define SAVE                    "SAVE"
-#define HELP                    "HELP"
-#define RULES                   "RULES"
-#define EXIT                    "EXIT"
-#define BROKEN                  "BROKEN"
-#define TURN_ARGC               4
-#define SAVE_ARGC               2
-#define MAX_FACTORY_TILES       4
-#define MAX_FACTORY_INSTANCES   5
-#define MAX_DISCARD_TILES       17
-#define MAX_TOTAL_TILES         100
-#define MAX_AMOUNT_COLOUR       20
-#define MAX_PLAYER_INSTANCES    2
-#define MAX_BROKEN_TILES        7
-#define MAX_BOARD_ROWS          5
-#define MAX_BOARD_COLS          5
+#define BLANK_SPACE ' '
+#define EMPTY '.'
+#define FIRST_PLAYER_TILE 'F'
+#define INPUT_TAB "> "
+#define SAVE_PATH "../save_files/"
+#define FILE_EXTENSION ".txt"
+#define TURN "TURN"
+#define SAVE "SAVE"
+#define HELP "HELP"
+#define RULES "RULES"
+#define EXIT "EXIT"
+#define BROKEN "BROKEN"
+#define TURN_ARGC 4
+#define SAVE_ARGC 2
+#define MAX_FACTORY_TILES 4
+#define MAX_FACTORY_INSTANCES 5
+#define MAX_DISCARD_TILES 17
+#define MAX_TOTAL_TILES 100
+#define MAX_AMOUNT_COLOUR 20
+#define MAX_PLAYER_INSTANCES 2
+#define MAX_BROKEN_TILES 7
+#define MAX_BOARD_ROWS 5
+#define MAX_BOARD_COLS 5
 
 typedef char Tile;
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
 
-using std::string;
 using std::getline;
 using std::ifstream;
+using std::string;
 
+using std::make_shared;
+using std::make_unique;
 using std::move;
 using std::shared_ptr;
-using std::make_shared;
 using std::unique_ptr;
-using std::make_unique;
 
 #endif //APT_A2_TYPES_H
