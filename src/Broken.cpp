3f29f17e733aa7c6c81801a7d9aa1854f417f8aa
[Moditha Sulakshana s3756843] <s3756843@student.rmit.edu.au>
2020-05-20 13:29:58 +1000

    update test cases

--- src/Broken.cpp
+++ src/Broken.cpp
@@ -2,47 +2,66 @@
 
 Broken::Broken() = default;
 
-Broken::Broken(std::basic_string<char> input) {
-    for (unsigned int i = 0; i != input.size(); ++i) {
+Broken::Broken(std::basic_string<char> input)
+{
+    for (unsigned int i = 0; i != input.size(); ++i)
+    {
         add(input[i]);
     }
 }
 
 Broken::~Broken() = default;
 
-std::vector<Tile> Broken::getTiles() const {
+std::vector<Tile> Broken::getTiles() const
+{
     return tiles;
 }
 
-bool Broken::add(const Tile tile) {
+bool Broken::add(const Tile tile)
+{
     bool added = false;
-    if (tiles.size() < MAX_BROKEN_TILES) {
+    if (tiles.size() < MAX_BROKEN_TILES)
+    {
         tiles.push_back(tile);
         added = true;
     }
     return added;
 }
 
-string Broken::toString() const {
+string Broken::toString() const
+{
     string toString;
     unsigned int i = 0;
-    while (i != tiles.size()) {
+    while (i != tiles.size())
+    {
         toString += tiles.at(i);
         ++i;
     }
     return toString;
 }
 
-unsigned int Broken::lostPoints() const {
+unsigned int Broken::lostPoints() const
+{
     unsigned int lostPoints = 0;
-    for (unsigned int i = 0; i != tiles.size(); ++i) {
-        if (i < 2) { lostPoints += 1; }
-        else if (i < 5) { lostPoints += 2; }
-        else { lostPoints += 3; }
+    for (unsigned int i = 0; i != tiles.size(); ++i)
+    {
+        if (i < 2)
+        {
+            lostPoints += 1;
+        }
+        else if (i < 5)
+        {
+            lostPoints += 2;
+        }
+        else
+        {
+            lostPoints += 3;
+        }
     }
     return lostPoints;
 }
 
-void Broken::clear() {
+void Broken::clear()
+{
     tiles.clear();
 }
